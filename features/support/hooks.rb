019a71e6f2d4cb348be7820bae46d07d9ea10473
Yasuhito Takamiya <yasuhito@gmail.com>
2016-08-30 14:57:48 +0900

    Refactoring

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -2,13 +2,9 @@
 
 Before do
   Dir.chdir(Aruba.config.working_directory) do
-    FileUtils.mkdir_p('./log') unless File.exist?('./log')
-    FileUtils.mkdir_p('./pids') unless File.exist?('./pids')
-    FileUtils.mkdir_p('./sockets') unless File.exist?('./sockets')
-
-    Phut.log_dir = './log'
-    Phut.pid_dir = './pids'
-    Phut.socket_dir = './sockets'
+    NetTester.log_dir = './log'
+    NetTester.pid_dir = './pids'
+    NetTester.socket_dir = './sockets'
 
     NetTester.run(network_device: 'eth1', physical_switch_dpid: 0x123)
   end
@@ -16,10 +12,6 @@ end
 
 After do
   Dir.chdir(Aruba.config.working_directory) do
-    Phut.log_dir = './log'
-    Phut.pid_dir = './pids'
-    Phut.socket_dir = './sockets'
-
     NetTester.kill
   end
 end
