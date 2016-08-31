c9059df2f5a125a3a2becff464b91fa4c638c42d
Yasuhito Takamiya <yasuhito@gmail.com>
2016-08-31 13:53:24 +0900

    Don't use FactoryGirl's sequence directive

--- features/factories.rb
+++ features/factories.rb
@@ -4,15 +4,15 @@ FactoryGirl.define do
   trait :internal_network_host do
     netmask '255.255.255.0'
     gateway '10.10.10.254'
-    sequence(:ip_address, 3) { |n| "10.10.10.#{n}" }
     mac_address Faker::Internet.mac_address('00')
-
-    sequence :virtual_port_number, 2
-    sequence :physical_port_number, 2
   end
 
-  factory :ntp_client, class: NetTester::Netns do
-    name 'ntp_client'
+  factory :ntp_client, class: Netns do
     internal_network_host
+
+    name 'ntp_client'
+    ip_address '10.10.10.3'
+    virtual_port_number 2
+    physical_port_number 2
   end
 end
