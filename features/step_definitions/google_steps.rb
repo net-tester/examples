0a0d1ebb6a022b67db3d60c22348a32bd049866d
Kohei Yatsushiro <koheiyatsushiro@yahoo.co.jp>
2016-09-09 17:14:58 +0900

    Refactoring

--- features/step_definitions/google_steps.rb
+++ features/step_definitions/google_steps.rb
@@ -5,14 +5,16 @@ end
 
 Given(/^Google のページを起動するWebサーバ$/) do
   @google_pc = Netns.new(attributes_for(:google_pc))
+  cd('.') do
+    system "sudo mkdir -p /etc/netns/browser_pc"
+    system "sudo echo '10.10.10.4 google.com' > /etc/netns/browser_pc/hosts"
+    system "ip netns exec google_pc echo '<title>Google</title>' | ip netns exec google_pc nc -l -p 80 > log/nc.log &"
+  end
 end
 
 When(/^ブラウザで Google のページを開く$/) do
-  system "sudo mkdir -p /etc/netns/browser_pc"
-  system "sudo echo '10.10.10.4 google.com' > /etc/netns/browser_pc/hosts"
   system "sudo ovs-ofctl add-flow test_0xdad1c001 dl_dst=ff:ff:ff:ff:ff:ff,actions=output:FLOOD"
   cd('.') do
-    system "ip netns exec google_pc echo '<title>Google</title>' | ip netns exec google_pc nc -l -p 80 > log/nc.log &"
     @browser_pc.exec 'curl -L http://google.com/ | iconv -f SHIFT-JIS -t UTF8 > log/google.log'
   end
 end
