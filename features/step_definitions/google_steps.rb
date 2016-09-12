64571bd0c59bc28a5976494d3a0a3f26845eb378
Kohei Yatsushiro <koheiyatsushiro@yahoo.co.jp>
2016-09-12 13:50:15 +0900

    Use openssl for https

--- features/step_definitions/google_steps.rb
+++ features/step_definitions/google_steps.rb
@@ -8,14 +8,16 @@ Given(/^Google のページを起動するWebサーバ$/) do
   cd('.') do
     system "sudo mkdir -p /etc/netns/browser_pc"
     system "sudo echo '10.10.10.4 google.com' > /etc/netns/browser_pc/hosts"
-    system "ip netns exec google_pc echo '<title>Google</title>' | ip netns exec google_pc nc -l -p 80 > log/nc.log &"
+    #system "ip netns exec google_pc echo '<title>Google</title>' | ip netns exec google_pc nc -l -p 443 > log/nc.log &"
+    system "yes '' | openssl req -x509 -newkey rsa:4096 -nodes -sha256 -keyout server.key -out server.crt -days 30"
+    system "ip netns exec google_pc echo '<title>Google</title>' | ip netns exec google_pc openssl s_server -cert server.crt -key server.key -accept 443 > log/nc.log &"
   end
 end
 
 When(/^ブラウザで Google のページを開く$/) do
   system "sudo ovs-ofctl add-flow test_0xdad1c001 dl_dst=ff:ff:ff:ff:ff:ff,actions=output:FLOOD"
   cd('.') do
-    @browser_pc.exec 'curl -L http://google.com/ | iconv -f SHIFT-JIS -t UTF8 > log/google.log'
+    @browser_pc.exec 'curl -L --insecure  https://google.com/ | iconv -f SHIFT-JIS -t UTF8 > log/google.log'
   end
 end
 
