ac222eb9ccf7e520289459ea0939af4c1b870e68
ko-he- <koheiyatsushiro@yahoo.co.jp>
2016-10-05 09:21:21 +0900

    Fix ssh user

--- features/step_definitions/nc_steps.rb
+++ features/step_definitions/nc_steps.rb
@@ -1,8 +1,8 @@
 When(/^ヨーヨーダイン社からヨーヨーダイン社内部の資産管理サーバにsshでログイン$/) do  
   cd('.') do
-    @asset_server.exec "ssh-keygen -f ./ssh-key -t rsa -b 2048 -N ''"
-    @asset_server.exec "/usr/sbin/sshd -o AuthorizedKeysFile=$PWD/ssh-key.pub"
-    @internal_pc.exec "bash -c 'ssh -t -t #{@asset_server.ip_address} -i ./ssh-key -o StrictHostKeyChecking=no ip a ' > log/ssh.log"
+    @internal_pc.exec "sudo ssh-keygen -f ./ssh-key -t rsa -b 2048 -N ''"
+    @asset_server.exec "sudo /usr/sbin/sshd -o AuthorizedKeysFile=$PWD/ssh-key.pub -o PubkeyAuthentication=yes"
+    @internal_pc.exec "bash -c 'sudo ssh -t -t $SUDO_USER@#{@asset_server.ip_address} -i ./ssh-key -o StrictHostKeyChecking=no ip a ' > log/ssh.log"
   end
 end
 
