f9ca2cd8325443fb01c81d301ad39020509c959a
ko-he- <koheiyatsushiro@yahoo.co.jp>
2016-10-11 13:35:17 +0900

    Merge

--- features/step_definitions/virtual_host.rb
+++ features/step_definitions/virtual_host.rb
@@ -13,3 +13,7 @@ end
 Given(/^ヨーヨーダイン社のDMZ内のDNSサーバ$/) do
   @dns_server = Netns.new(attributes_for(:dns_server))
 end
+
+Given(/^ヨーヨーダイン社のDMZ内部のサーバ$/) do
+  @dmz_server = Netns.new(attributes_for(:dmz_server))
+end
