380e94c51b0a0dddc7115ed8834a61deea42a84e
127.0.0.1 <qb0C80aE@gmail.com>
2016-10-20 09:52:07 +0900

    Add teardown (#10)

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'active_support/core_ext/object/try'
+require File.expand_path(__dir__ + '/teardown/run_command.rb')
 
 Before do
   NetTester.log_dir = File.join(Aruba.config.working_directory, 'log')
@@ -14,4 +15,7 @@ end
 
 After do
   NetTester.kill
+  cmd2hosts = RunCommands2Hosts.new
+  cmd2hosts.run_command_for_all_hosts('c3750g_hosts.yml', 'c3750g_teardown.yml')
+  cmd2hosts.run_command_for_all_hosts('ssg_hosts.yml', 'ssg_teardown.yml')
 end
