96e46b8be1818703079b3c6a77d72bbcc1faf7fb
qb0C80aE <qb0C80aE@gmail.com>
2016-12-22 16:39:21 +0900

    Implement the telnet feature from the vpn address pool to the internal network

--- features/step_definitions/telnet_steps.rb
+++ features/step_definitions/telnet_steps.rb
@@ -6,6 +6,14 @@ When(/^開発者 PC からテストサーバへTelnetでアクセス$/) do
   end
 end
 
+When(/^ヨーヨーダイン社のDMZ内部のVPNアドレスプールからヨーヨーダイン社内部のテスト環境サーバへTelnetでアクセス$/) do
+  cd('.') do
+    @async_test_server = AsyncExecutor.new(host: @test_server, result_file: 'log/telnet_server.log')
+    @async_test_server.exec "nc -l 23"
+    @vpn_address_pool.exec "bash -c 'telnet #{@test_server.ip_address} > log/telnet_client.log; exit 0'"
+  end
+end
+
 Then(/^社内テストサーバにアクセス成功$/) do
   step %(the file "log/telnet_client.log" should contain "Escape character is")
 end
