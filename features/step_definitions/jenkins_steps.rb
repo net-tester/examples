505f1e3b83a1716f09a82e7837f012266693028b
127.0.0.1 <qb0C80aE@gmail.com>
2017-03-03 17:22:37 +0900

    Refactor features related to DNS service (#46)

--- features/step_definitions/jenkins_steps.rb
+++ features/step_definitions/jenkins_steps.rb
@@ -1,20 +1,8 @@
 # coding: utf-8
-When(/^開発者 PC から Web ブラウザでテストサーバの Web コンソールにアクセス$/) do
+When(/^Web ブラウザでヨーヨーダイン社内部のテスト環境サーバの Web コンソールにアクセス$/) do
   cd('.') do
     @jenkins_service = AsyncExecutor.new(host: @test_host, result_file: 'log/test_host.log')
-    @jenkins_service.exec "bash -c 'echo -e \"HTTP/1.1 200 OK\\n\\nJenkins\" | nc -l 13000'"
-    @internal_pc.exec "curl http://#{@test_host.ip_address}:13000/ > log/client.log"
+    @jenkins_service.exec "bash -c 'echo -e \"HTTP/1.1 200 OK\\n\\nAccessOK\" | nc -l 13000'"
+    @src_host.exec "curl http://#{@test_host.ip_address}:13000/ > log/access.log"
   end
-end
-
-When(/^ヨーヨーダイン社のDMZ内部のVPNアドレスプールから Web ブラウザでヨーヨーダイン社内部のテスト環境サーバの Web コンソールにアクセス$/) do
-  cd('.') do
-    @jenkins_service = AsyncExecutor.new(host: @test_host, result_file: 'log/test_host.log')
-    @jenkins_service.exec "bash -c 'echo -e \"HTTP/1.1 200 OK\\n\\nJenkins\" | nc -l 13000'"
-    @vpn_address_pool.exec "curl http://#{@test_host.ip_address}:13000/ > log/client.log"
-  end
-end
-
-Then(/^社内テストサーバの Web コンソールにアクセス成功$/) do
-  step %(the file "log/client.log" should contain "Jenkins")
 end
