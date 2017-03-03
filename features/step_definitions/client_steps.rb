450a9cf254dfa8b5f67c55f64e9f569a664d2972
127.0.0.1 <qb0C80aE@gmail.com>
2017-03-03 17:14:08 +0900

    Refactor features related to DNS service (#45)

---
+++ features/step_definitions/client_steps.rb
@@ -0,0 +1,13 @@
+# coding: utf-8
+When(/^ヨーヨーダイン社内部のユーザ PC にログイン$/) do
+  @src_host = @user_pc
+end
+
+When(/^ヨーヨーダイン社の DMZ のサーバにログイン$/) do
+  @src_host = @dmz_host
+end
+
+When(/^ヨーヨーダイン社の DMZ の DNS サーバにログイン$/) do
+  @src_host = @dns_host
+end
+
