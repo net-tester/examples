fb338ce7b78607f89d755ba9d7493e0e16cfd1b8
127.0.0.1 <qb0C80aE@gmail.com>
2017-03-03 17:22:37 +0900

    Refactor features related to DNS service (#46)

--- features/step_definitions/client_steps.rb
+++ features/step_definitions/client_steps.rb
@@ -3,11 +3,6 @@ When(/^ヨーヨーダイン社内部のユーザ PC にログイン$/) do
   @src_host = @user_pc
 end
 
-When(/^ヨーヨーダイン社の DMZ のサーバにログイン$/) do
-  @src_host = @dmz_host
+When(/^ヨーヨーダイン社の DMZ の VPN アドレスプールにログイン$/) do
+  @src_host = @vpn_address_pool
 end
-
-When(/^ヨーヨーダイン社の DMZ の DNS サーバにログイン$/) do
-  @src_host = @dns_host
-end
-
