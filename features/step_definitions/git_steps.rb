d9d62593f57710c5efcadb851b684ed49ab34f05
127.0.0.1 <qb0C80aE@gmail.com>
2017-03-03 18:38:50 +0900

    Refactor features related to Git service (#48)

--- features/step_definitions/git_steps.rb
+++ features/step_definitions/git_steps.rb
@@ -1,31 +1,9 @@
 # coding: utf-8
-
-Given(/^ヨーヨーダイン社 の Git リポジトリサーバ$/) do
-  @git_host = Netns.new(attributes_for(:git_host))
-  cd('.') do
-    @git_service = AsyncExecutor.new(host: @git_host, result_file: 'log/git_host.log')
-    @git_service.exec "bash -c 'echo OK | nc -l 11000'"
-  end
-end
-
-Given(/^Git クライアントとなる開発者 PC$/) do
-  @git_client = Netns.new(attributes_for(:git_client))
-end
-
-When(/^開発者 PC から社内 Git リポジトリへアクセス$/) do
-  cd('.') do
-    @git_client.exec "nc -v #{@git_host.ip_address} 11000 > log/nc_git.log"
-  end
-end
-
 When(/^資産管理サーバへ git$/) do
   cd('.') do
     @asset_service = AsyncExecutor.new(host: @asset_host, result_file: 'log/asset_host.log')
     @asset_service.exec "bash -c 'echo AccessOK | nc -l 11000'"
-    @vpn_address_pool.exec "nc -v #{@asset_host.ip_address} 11000 > log/access.log"
+    @src_host.exec "nc -v #{@asset_host.ip_address} 11000 > log/access.log"
   end
 end
 
-Then(/^社内 Git リポジトリにアクセス成功$/) do
-  step %(the file "log/nc_git.log" should contain "OK")
-end
