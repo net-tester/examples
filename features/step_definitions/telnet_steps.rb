8db9bf5efd189bda5512d5e5cb7f28e311b759e7
qb0C80aE <qb0C80aE@gmail.com>
2017-03-06 15:02:32 +0900

    Refactor features related to Telnet service

--- features/step_definitions/telnet_steps.rb
+++ features/step_definitions/telnet_steps.rb
@@ -1,20 +1,8 @@
 # coding: utf-8
-When(/^開発者 PC からテストサーバへTelnetでアクセス$/) do
+When(/^ヨーヨーダイン社内部のテスト環境サーバに telnet でログイン$/) do
   cd('.') do
     @telnet_service = AsyncExecutor.new(host: @test_host, result_file: 'log/telnet_host.log')
-    @telnet_service.exec "bash -c 'echo TelnetOK | sudo nc -l 23'"
-    @internal_pc.exec "bash -c 'telnet #{@test_host.ip_address} > log/telnet_client.log; exit 0'"
+    @telnet_service.exec "bash -c 'echo LoginOK | sudo nc -l 23'"
+    @src_host.exec "ruby -e \"require 'net/telnet'; Net::Telnet.new('Host' => '#{@test_host.ip_address}', 'Port' => '23').cmd('') {|res| print res}\" > log/login.log"
   end
-end
-
-When(/^ヨーヨーダイン社のDMZ内部のVPNアドレスプールからヨーヨーダイン社内部のテスト環境サーバへTelnetでアクセス$/) do
-  cd('.') do
-    @telnet_service = AsyncExecutor.new(host: @test_host, result_file: 'log/telnet_host.log')
-    @telnet_service.exec "bash -c 'echo TelnetOK | sudo nc -l 23'"
-    @vpn_address_pool.exec "bash -c 'telnet #{@test_host.ip_address} > log/telnet_client.log; exit 0'"
-  end
-end
-
-Then(/^社内テストサーバにアクセス成功$/) do
-  step %(the file "log/telnet_client.log" should contain "TelnetOK")
 end
