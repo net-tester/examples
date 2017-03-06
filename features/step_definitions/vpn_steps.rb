b43d0f3a60349a86a4fea72ebf44b3c813cd3d08
qb0C80aE <qb0C80aE@gmail.com>
2017-03-06 15:23:17 +0900

    Refactor features related to VPN service

--- features/step_definitions/vpn_steps.rb
+++ features/step_definitions/vpn_steps.rb
@@ -1,24 +1,19 @@
 # coding: utf-8
-Given(/^ヨーヨーダイン社のDMZ内部のVPNサーバとVPNプロセス$/) do
-  step %(ヨーヨーダイン社のDMZ内部のVPNサーバ)
+When(/^ヨーヨーダイン社の VPN サーバに Firewall 経由で http でログイン$/) do
   cd('.') do
-    system "echo '<title>YoyodyneVPN</title>' > login.html"
-    @http_service = AsyncExecutor.new(host: @vpn_host, result_file: 'log/http_host.stdout', stderr_file: 'log/http_host.stderr')
-    @http_service.exec("ruby -rwebrick -e 'WEBrick::HTTPServer.new(:DocumentRoot => \"./\", :Port => 80).start'")
-    @https_service = AsyncExecutor.new(host: @vpn_host, result_file: 'log/https_host.stdout', stderr_file: 'log/https_host.stderr')
-    @http_service.exec("ruby -rwebrick -rwebrick/https -e 'WEBrick::HTTPServer.new(:DocumentRoot => \"./\", :Port => 443, :SSLEnable => true, :SSLCertName => [[\"CN\", WEBrick::Utils::getservername]] ).start'")
-    sleep 3;
+    system "echo '<title>LoginOK</title>' > login.html"
+    @vpn_service = AsyncExecutor.new(host: @vpn_host, result_file: 'log/http_host.stdout', stderr_file: 'log/http_host.stderr')
+    @vpn_service.exec("ruby -rwebrick -e 'WEBrick::HTTPServer.new(:DocumentRoot => \"./\", :Port => 80).start'")
+    @src_host.exec 'curl -L http://203.0.113.5/login.html > log/login.log'
   end
 end
 
-When(/^SSLVPNサーバにタジマックス工業のPCからログイン$/) do
+When(/^ヨーヨーダイン社の VPN サーバに Firewall 経由で https でログイン$/) do
   cd('.') do
-    @tajimax_pc.exec 'curl -L http://203.0.113.5/login.html > log/vpn_access.http'
-    @tajimax_pc.exec 'curl -L --insecure https://203.0.113.5/login.html > log/vpn_access.https'
+    system "echo '<title>LoginOK</title>' > login.html"
+    @vpn_service = AsyncExecutor.new(host: @vpn_host, result_file: 'log/https_host.stdout', stderr_file: 'log/https_host.stderr')
+    @vpn_service.exec("ruby -rwebrick -rwebrick/https -e 'WEBrick::HTTPServer.new(:DocumentRoot => \"./\", :Port => 443, :SSLEnable => true, :SSLCertName => [[\"CN\", WEBrick::Utils::getservername]] ).start'")
+    @src_host.exec 'curl -L --insecure https://203.0.113.5/login.html > log/login.log'
   end
 end
 
-Then(/^SSLVPNサーバにログイン成功$/) do
-  step %(the file "log/vpn_access.http" should contain "<title>YoyodyneVPN</title>")
-  step %(the file "log/vpn_access.https" should contain "<title>YoyodyneVPN</title>")
-end
