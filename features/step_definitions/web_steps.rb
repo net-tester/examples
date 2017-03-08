4c0ae83604d84e26eeb1931df35447cdcbf1cc13
tjmtrhs <tjmtrhs+github@gmail.com>
2017-03-08 13:04:34 +0900

    remove netns exec, use WEBrick (#58)
    * Remove netns exec (remove hard cording)
    * Use WEBrick instead of netcat

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -1,51 +1,54 @@
 # coding: utf-8
 When(/^ヨーヨーダイン社の VPN サーバに Web ブラウザから http でログイン$/) do
   cd('.') do
+    system "echo '<title>LoginOK</title>' > index.html"
     @vpn_service = AsyncExecutor.new(host: @vpn_host, result_file: 'log/vpn_host.log')
-    @vpn_service.exec "bash -c 'echo -e \"HTTP/1.1 200 OK\\n\\nLoginOK\" | nc -l 80'"
+    @vpn_service.exec("ruby -rwebrick -e 'WEBrick::HTTPServer.new(:DocumentRoot => \"./\", :Port => 80).start'")
     @src_host.exec "curl http://#{@vpn_host.ip_address}/ > log/login.log"
   end
 end
 
 When(/^ヨーヨーダイン社の VPN サーバに Web ブラウザから https でログイン$/) do
   cd('.') do
-    system "sudo yes '' | sudo openssl req -x509 -newkey rsa:4096 -nodes -sha256 -keyout server.key -out server.crt -days 30"
-    system "sudo ip netns exec #{@vpn_host.name} echo '<title>LoginOK</title>' | sudo ip netns exec #{@vpn_host.name} openssl s_server -cert server.crt -key server.key -accept 443 > log/vpn_host.log &"
-    sleep 2
+    system "echo '<title>LoginOK</title>' > index.html"
+    @vpn_service = AsyncExecutor.new(host: @vpn_host, result_file: 'log/vpn_host.log')
+    @vpn_service.exec("ruby -rwebrick -rwebrick/https -e 'WEBrick::HTTPServer.new(:DocumentRoot => \"./\", :Port => 443, :SSLEnable => true, :SSLCertName => [[\"CN\", WEBrick::Utils::getservername]] ).start'")
     @src_host.exec "wget --no-check-certificate https://#{@vpn_host.ip_address}/ -O log/login.log"
   end
 end
 
 When(/^ヨーヨーダイン社の DMZ のサーバに Web ブラウザから http でログイン$/) do
   cd('.') do
+    system "echo '<title>LoginOK</title>' > index.html"
     @http_service = AsyncExecutor.new(host: @dmz_host, result_file: 'log/dmz_host.log')
-    @http_service.exec "bash -c 'echo -e \"HTTP/1.1 200 OK\\n\\nLoginOK\" | nc -l 80'"
+    @http_service.exec("ruby -rwebrick -e 'WEBrick::HTTPServer.new(:DocumentRoot => \"./\", :Port => 80).start'")
     @src_host.exec "curl http://#{@dmz_host.ip_address}/ > log/login.log"
   end
 end
 
 When(/^ヨーヨーダイン社の DMZ のサーバに Web ブラウザから https でログイン$/) do
   cd('.') do
-    system "sudo yes '' | sudo openssl req -x509 -newkey rsa:4096 -nodes -sha256 -keyout server.key -out server.crt -days 30"
-    system "sudo ip netns exec dmz_host echo '<title>LoginOK</title>' | sudo ip netns exec dmz_host openssl s_server -cert server.crt -key server.key -accept 443 > log/dmz_host.log &"
-    sleep 2
+    system "echo '<title>LoginOK</title>' > index.html"
+    @dmz_service = AsyncExecutor.new(host: @dmz_host, result_file: 'log/dmz_host.log')
+    @dmz_service.exec("ruby -rwebrick -rwebrick/https -e 'WEBrick::HTTPServer.new(:DocumentRoot => \"./\", :Port => 443, :SSLEnable => true, :SSLCertName => [[\"CN\", WEBrick::Utils::getservername]] ).start'")
     @src_host.exec "wget --no-check-certificate https://#{@dmz_host.ip_address}/ -O log/login.log"
   end
 end
 
 When(/^http でインターネット上のサーバからパッケージアップデートを実行$/) do
   cd('.') do
+    system "echo '<title>UpdateOK</title>' > index.html"
     @http_service = AsyncExecutor.new(host: @internet_host, result_file: 'log/internet_host.log')
-    @http_service.exec "bash -c 'echo -e \"HTTP/1.1 200 OK\\n\\nUpdateOK\" | nc -l 80'"
+    @http_service.exec("ruby -rwebrick -e 'WEBrick::HTTPServer.new(:DocumentRoot => \"./\", :Port => 80).start'")
     @src_host.exec "curl http://#{@internet_host.ip_address}/ > log/update.log"
   end
 end
 
 When(/^https でインターネット上のサーバからパッケージアップデートを実行$/) do
   cd('.') do
-    system "sudo yes '' | sudo openssl req -x509 -newkey rsa:4096 -nodes -sha256 -keyout server.key -out server.crt -days 30"
-    system "sudo ip netns exec #{@internet_host.name} echo '<title>UpdateOK</title>' | sudo ip netns exec #{@internet_host.name} openssl s_server -cert server.crt -key server.key -accept 443 > log/internet_host.log &"
-    sleep 2
+    system "echo '<title>UpdateOK</title>' > index.html"
+    @https_service = AsyncExecutor.new(host: @internet_host, result_file: 'log/internet_host.log')
+    @https_service.exec("ruby -rwebrick -rwebrick/https -e 'WEBrick::HTTPServer.new(:DocumentRoot => \"./\", :Port => 443, :SSLEnable => true, :SSLCertName => [[\"CN\", WEBrick::Utils::getservername]] ).start'")
     @src_host.exec "wget --no-check-certificate https://#{@internet_host.ip_address}/ -O log/update.log"
   end
 end
