50f124df4a8147eff7c658f993d2d0652b77af2e
qb0C80aE <qb0C80aE@gmail.com>
2017-04-05 15:32:18 +0900

    Unifiy LoginOK, UpdateOK, AccessOK into AccessOK

--- features/step_definitions/vpn_steps.rb
+++ features/step_definitions/vpn_steps.rb
@@ -1,19 +1,19 @@
 # coding: utf-8
 When(/^VPN サーバに Firewall 経由で http でログイン$/) do
   cd('.') do
-    system "echo '<title>LoginOK</title>' > login.html"
+    system "echo '<title>AccessOK</title>' > login.html"
     @vpn_service = AsyncExecutor.new(host: @vpn_host, result_file: 'log/http_host.stdout', stderr_file: 'log/http_host.stderr')
     @vpn_service.exec("ruby -rwebrick -e 'WEBrick::HTTPServer.new(:DocumentRoot => \"./\", :Port => 80).start'")
-    @src_host.exec 'curl -L http://203.0.113.5/login.html > log/login.log'
+    @src_host.exec 'curl -L http://203.0.113.5/login.html > log/access.log'
   end
 end
 
 When(/^VPN サーバに Firewall 経由で https でログイン$/) do
   cd('.') do
-    system "echo '<title>LoginOK</title>' > login.html"
+    system "echo '<title>AccessOK</title>' > login.html"
     @vpn_service = AsyncExecutor.new(host: @vpn_host, result_file: 'log/https_host.stdout', stderr_file: 'log/https_host.stderr')
     @vpn_service.exec("ruby -rwebrick -rwebrick/https -e 'WEBrick::HTTPServer.new(:DocumentRoot => \"./\", :Port => 443, :SSLEnable => true, :SSLCertName => [[\"CN\", WEBrick::Utils::getservername]] ).start'")
-    @src_host.exec 'curl -L --insecure https://203.0.113.5/login.html > log/login.log'
+    @src_host.exec 'curl -L --insecure https://203.0.113.5/login.html > log/access.log'
   end
 end
 
--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -1,55 +1,55 @@
 # coding: utf-8
 When(/^VPN サーバに Web ブラウザから http でログイン$/) do
   cd('.') do
-    system "echo '<title>LoginOK</title>' > index.html"
+    system "echo '<title>AccessOK</title>' > index.html"
     @vpn_service = AsyncExecutor.new(host: @vpn_host, result_file: 'log/vpn_host.log')
     @vpn_service.exec("ruby -rwebrick -e 'WEBrick::HTTPServer.new(:DocumentRoot => \"./\", :Port => 80).start'")
-    @src_host.exec "curl http://#{@vpn_host.ip_address}/ > log/login.log"
+    @src_host.exec "curl http://#{@vpn_host.ip_address}/ > log/access.log"
   end
 end
 
 When(/^VPN サーバに Web ブラウザから https でログイン$/) do
   cd('.') do
-    system "echo '<title>LoginOK</title>' > index.html"
+    system "echo '<title>AccessOK</title>' > index.html"
     @vpn_service = AsyncExecutor.new(host: @vpn_host, result_file: 'log/vpn_host.log')
     @vpn_service.exec("ruby -rwebrick -rwebrick/https -e 'WEBrick::HTTPServer.new(:DocumentRoot => \"./\", :Port => 443, :SSLEnable => true, :SSLCertName => [[\"CN\", WEBrick::Utils::getservername]] ).start'")
-    @src_host.exec "wget --no-check-certificate https://#{@vpn_host.ip_address}/ -O log/login.log"
+    @src_host.exec "wget --no-check-certificate https://#{@vpn_host.ip_address}/ -O log/access.log"
   end
 end
 
 When(/^DMZ のサーバに Web ブラウザから http でログイン$/) do
   cd('.') do
-    system "echo '<title>LoginOK</title>' > index.html"
+    system "echo '<title>AccessOK</title>' > index.html"
     @http_service = AsyncExecutor.new(host: @dmz_host, result_file: 'log/dmz_host.log')
     @http_service.exec("ruby -rwebrick -e 'WEBrick::HTTPServer.new(:DocumentRoot => \"./\", :Port => 80).start'")
-    @src_host.exec "curl http://#{@dmz_host.ip_address}/ > log/login.log"
+    @src_host.exec "curl http://#{@dmz_host.ip_address}/ > log/access.log"
   end
 end
 
 When(/^DMZ のサーバに Web ブラウザから https でログイン$/) do
   cd('.') do
-    system "echo '<title>LoginOK</title>' > index.html"
+    system "echo '<title>AccessOK</title>' > index.html"
     @dmz_service = AsyncExecutor.new(host: @dmz_host, result_file: 'log/dmz_host.log')
     @dmz_service.exec("ruby -rwebrick -rwebrick/https -e 'WEBrick::HTTPServer.new(:DocumentRoot => \"./\", :Port => 443, :SSLEnable => true, :SSLCertName => [[\"CN\", WEBrick::Utils::getservername]] ).start'")
-    @src_host.exec "wget --no-check-certificate https://#{@dmz_host.ip_address}/ -O log/login.log"
+    @src_host.exec "wget --no-check-certificate https://#{@dmz_host.ip_address}/ -O log/access.log"
   end
 end
 
 When(/^http でパッケージアップデートを実行$/) do
   cd('.') do
-    system "echo '<title>UpdateOK</title>' > index.html"
+    system "echo '<title>AccessOK</title>' > index.html"
     @http_service = AsyncExecutor.new(host: @internet_host, result_file: 'log/internet_host.log')
     @http_service.exec("ruby -rwebrick -e 'WEBrick::HTTPServer.new(:DocumentRoot => \"./\", :Port => 80).start'")
-    @src_host.exec "curl http://#{@internet_host.ip_address}/ > log/update.log"
+    @src_host.exec "curl http://#{@internet_host.ip_address}/ > log/access.log"
   end
 end
 
 When(/^https でパッケージアップデートを実行$/) do
   cd('.') do
-    system "echo '<title>UpdateOK</title>' > index.html"
+    system "echo '<title>AccessOK</title>' > index.html"
     @https_service = AsyncExecutor.new(host: @internet_host, result_file: 'log/internet_host.log')
     @https_service.exec("ruby -rwebrick -rwebrick/https -e 'WEBrick::HTTPServer.new(:DocumentRoot => \"./\", :Port => 443, :SSLEnable => true, :SSLCertName => [[\"CN\", WEBrick::Utils::getservername]] ).start'")
-    @src_host.exec "wget --no-check-certificate https://#{@internet_host.ip_address}/ -O log/update.log"
+    @src_host.exec "wget --no-check-certificate https://#{@internet_host.ip_address}/ -O log/access.log"
   end
 end
 
