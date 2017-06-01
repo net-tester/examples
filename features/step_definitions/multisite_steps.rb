2f67ff53fb8ab6738f908055d994bddc881affa8
tjmtrhs <tjmtrhs+github@gmail.com>
2017-06-01 16:25:03 +0900

    make http_client global instance

--- features/step_definitions/multisite_steps.rb
+++ features/step_definitions/multisite_steps.rb
@@ -1,9 +1,6 @@
 # coding: utf-8
-require 'httpclient'
 
 Given(/^VLAN ID (\d+) のユーザグループ$/) do |vlan_id, table|
-  http_client = HTTPClient.new
-
   table.hashes.each do |each|
     attributes = attributes_for("#{each['ノード']}_host".to_sym)
     attributes[:vlan_id] = vlan_id.to_i
@@ -13,7 +10,7 @@ Given(/^VLAN ID (\d+) のユーザグループ$/) do |vlan_id, table|
 
     tester_set = tester_sets[each['拠点']]
     apiroot = "http://" + tester_set[:ip_address] + ":3000/"
-    http_client.put(apiroot + "hosts/#{each['ノード']}", attributes.to_json, 'Content-Type' => 'application/json')
+    @http_client.put(apiroot + "hosts/#{each['ノード']}", attributes.to_json, 'Content-Type' => 'application/json')
     # TODO: error catch
   end
 end
@@ -27,7 +24,6 @@ Given(/^通信要件表$/) do |table|
 end
 
 When(/^通信要件どおりに ping$/) do
-  http_client = HTTPClient.new
   @requirements.each do |requirement|
     src_host_name = requirement[:src]
     dest_host_name = requirement[:dest]
@@ -41,22 +37,20 @@ When(/^通信要件どおりに ping$/) do
       :host_name => src_host_name,
       :command => "bash -c 'ping #{dest_host[:ip_address]} -c 1; exit 0'"
     }
-    http_client.post(apiroot+"processes", src_host_command.to_json, 'Content-Type' => 'application/json')
+    @http_client.post(apiroot+"processes", src_host_command.to_json, 'Content-Type' => 'application/json')
 
     # 本チャン
     src_host_command = {
       :host_name => src_host_name,
       :command => "ping #{dest_host[:ip_address]} -c 4"
     }
-    res = http_client.post(apiroot+"processes", src_host_command.to_json, 'Content-Type' => 'application/json')
+    res = @http_client.post(apiroot+"processes", src_host_command.to_json, 'Content-Type' => 'application/json')
     result = JSON.parse(res.body)
     requirement[:id] = result["id"]
   end
 end
 
 Then(/^通信要件どおりに ping 成功$/) do
-  http_client = HTTPClient.new
-
   @requirements.each do |requirement|
     src_host_name = requirement[:src]
     dest_host_name = requirement[:dest]
@@ -67,7 +61,7 @@ Then(/^通信要件どおりに ping 成功$/) do
 
     apiroot = "http://" + tester_set[:ip_address] + ":3000/"
     while(1) do
-      res = http_client.get(apiroot + "processes/" + requirement[:id])
+      res = @http_client.get(apiroot + "processes/" + requirement[:id])
       result = JSON.parse(res.body)
       if(result["status"] == "finished") then
         break
--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -2,28 +2,28 @@
 # frozen_string_literal: true
 
 require 'active_support/core_ext/object/try'
-require 'expectacle'
-require 'net/ssh'
 require 'httpclient'
 
 # tester setは１箇所定義にしたいのでこうしている
 require File.expand_path("../tester_sets.rb", __FILE__)
 
 Before do
+  @http_client = HTTPClient.new
+  # HTTP req/res 表示用
+  # @http_client.debug_dev = $stderr
+
   # TODO: work-around なぜか初回にいきなりhostを作るとISEになるので、一回削除を挟んでおく
   tester_sets.each{|_, tester_set|
-    client = HTTPClient.new
     apiroot = "http://" + tester_set[:ip_address] + ":3000/"
-    res = client.delete(apiroot + "sites")
+    res = @http_client.delete(apiroot + "sites")
   }
   # TODO: エラーチェック
 end
 
 After do
   tester_sets.each{|_, tester_set|
-    client = HTTPClient.new
     apiroot = "http://" + tester_set[:ip_address] + ":3000/"
-    res = client.delete(apiroot + "sites")
+    res = @http_client.delete(apiroot + "sites")
   }
   # TODO: エラーチェック
 end
