28ffcff6dbcac6ecbc5405795ab1acdea1b87452
qb0C80aE <qb0C80aE@gmail.com>
2017-12-20 17:55:39 +0900

    Modify git_steps to make it work under TestNode

--- features/step_definitions/git_steps.rb
+++ features/step_definitions/git_steps.rb
@@ -1,9 +1,9 @@
 # coding: utf-8
 When(/^資産管理サーバへ git$/) do
   cd('.') do
-    @asset_service = AsyncExecutor.new(host: @asset_host, result_file: 'log/asset_host.log')
-    @asset_service.exec "bash -c 'echo AccessOK | nc -l 11000'"
-    @src_host.exec "nc -v #{@asset_host.ip_address} 11000 > log/access.log"
+    @asset_service = @asset_host
+    @asset_service.exec("bash -c 'echo AccessOK | nc -l 11000'")
+    @process_id = @src_host.exec("nc -v #{@asset_host.ip_address} 11000", delayed: true)
   end
 end
 
