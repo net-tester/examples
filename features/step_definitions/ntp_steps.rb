d10b198aaff5ad84593ddb8289e604ae07eeb10a
qb0C80aE <qb0C80aE@gmail.com>
2017-12-20 18:31:12 +0900

    Modify ntp_steps to make it work under TestNode

--- features/step_definitions/ntp_steps.rb
+++ features/step_definitions/ntp_steps.rb
@@ -1,22 +1,22 @@
 # coding: utf-8
 When(/^時刻をインターネット上の NTP サーバと同期$/) do
   cd('.') do
-    @src_host.exec 'ntpdate -q ntp.nict.jp > log/ntpdate.log'
+    @process_id = @src_host.exec('ntpdate -q ntp.nict.jp')
   end
 end
 
 When(/^時刻をインターネット上の NTP サーバと同期 \(UDP\)$/) do
   cd('.') do
-    @ntp_service = AsyncExecutor.new(host: @internet_ntp_host, result_file: 'log/internet_ntp_host.log')
-    @ntp_service.exec "bash -c 'echo -e \"adjust time server\" | nc -lu 123'"
-    @src_host.exec "nc -uv -w4 #{@internet_ntp_host.ip_address} 123 > log/ntpdate.log"
+    @ntp_service = @internet_ntp_host
+    @ntp_service.exec("bash -c 'echo -e \"adjust time server\" | nc -lu 123'")
+    @process_id = @src_host.exec("nc -uv -w4 #{@internet_ntp_host.ip_address} 123", delayed: true)
   end
 end
 
 When(/^時刻をインターネット上の NTP サーバと同期 \(TCP\)$/) do
   cd('.') do
-    @ntp_service = AsyncExecutor.new(host: @internet_ntp_host, result_file: 'log/internet_ntp_host.log')
-    @ntp_service.exec "bash -c 'echo -e \"adjust time server\" | nc -l 123'"
-    @src_host.exec "nc #{@internet_ntp_host.ip_address} 123 > log/ntpdate.log"
+    @ntp_service = @internet_ntp_host
+    @ntp_service.exec("bash -c 'echo -e \"adjust time server\" | nc -l 123'")
+    @process_id = @src_host.exec("nc #{@internet_ntp_host.ip_address} 123", delayed: true)
   end
 end
