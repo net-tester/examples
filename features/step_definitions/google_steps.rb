23d1c803ed2a97c51c3304e723eb9f9ffe663cd1
qb0C80aE <qb0C80aE@gmail.com>
2017-12-20 18:31:06 +0900

    Modify google_steps to make it work under TestNode

--- features/step_definitions/google_steps.rb
+++ features/step_definitions/google_steps.rb
@@ -1,17 +1,17 @@
 # coding: utf-8
 When(/^ブラウザで Google のページを開く$/) do
   cd('.') do
-    @src_host.exec 'curl -L --insecure  https://www.google.com/ | iconv -f SHIFT-JIS -t UTF8 > log/google.log'
+    @process_id = @src_host.exec('curl -L --insecure  https://www.google.com/ | iconv -f SHIFT-JIS -t UTF8')
   end
 end
 
 When(/^ブラウザでインターネット上のサーバの Google のページを開く$/) do
   cd('.') do
-    system "echo '<title>Google</title>' > index.html"
-    @https_service = AsyncExecutor.new(host: @internet_host, result_file: 'log/google_host.stdout', stderr_file: 'log/google_host.stderr')
+    @internet_host.exec("echo '<title>Google</title>' > index.html", sync: true)
+    @https_service = @internet_host
     @https_service.exec("ruby -rwebrick -rwebrick/https -e 'WEBrick::HTTPServer.new(:DocumentRoot => \"./\", :Port => 443, :SSLEnable => true, :SSLCertName => [[\"CN\", WEBrick::Utils::getservername]] ).start'")
-    system "sudo mkdir -p /etc/netns/#{@src_host.name}"
-    system "echo '198.51.100.3 www.google.com' | sudo tee /etc/netns/#{@src_host.name}/hosts >/dev/null"
-    @src_host.exec 'curl -L --insecure  https://www.google.com/ | iconv -f SHIFT-JIS -t UTF8 > log/google.log'
+    @src_host.exec("sudo mkdir -p /etc/netns/#{@src_host.name}", sync: true)
+    @src_host.exec("echo '198.51.100.3 www.google.com' | sudo tee /etc/netns/#{@src_host.name}/hosts >/dev/null", sync: true)
+    @process_id = @src_host.exec('curl -L --insecure  https://www.google.com/ | iconv -f SHIFT-JIS -t UTF8', delayed: true)
   end
 end
