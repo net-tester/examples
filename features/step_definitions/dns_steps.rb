973638b266aa36be205517c2351176e8acf8e73e
qb0C80aE <qb0C80aE@gmail.com>
2017-12-20 17:55:27 +0900

    Modify dns_steps to make it work under TestNode

--- features/step_definitions/dns_steps.rb
+++ features/step_definitions/dns_steps.rb
@@ -1,28 +1,24 @@
 # coding: utf-8
 When(/^DMZ の DNS サーバに dig コマンドで "www.google.com" の IP アドレスを問い合わせる$/) do
   cd('.') do
-    @dns_host.exec "mkdir -p /etc/netns/#{@dns_host.name}"
-    @dns_host.exec "echo '172.217.25.196 www.google.com' | sudo tee /etc/netns/#{@dns_host.name}/hosts"
-    @dns_host.exec "echo 'port=53' | sudo tee dnsmasq.conf"
-    @dns_host.exec "echo 'expand-hosts' | sudo tee -a dnsmasq.conf"
-    @dns_service = AsyncExecutor.new(host: @dns_host, result_file: 'log/dns.log')
-    @dns_service.exec "dnsmasq -C dnsmasq.conf"
-    @src_host.exec "dig @#{@dns_host.ip_address} www.google.com A > log/dns.log"
+    @dns_host.exec("mkdir -p /etc/netns/#{@dns_host.name}", sync: true)
+    @dns_host.exec("echo '172.217.25.196 www.google.com' | sudo tee /etc/netns/#{@dns_host.name}/hosts", sync: true)
+    @dns_host.exec("echo 'port=53' | sudo tee dnsmasq.conf", sync: true)
+    @dns_host.exec("echo 'expand-hosts' | sudo tee -a dnsmasq.conf", sync: true)
+    @dns_service = @dns_host
+    @dns_service.exec("dnsmasq -C dnsmasq.conf")
+    @process_id = @src_host.exec("dig @#{@dns_host.ip_address} www.google.com A", delayed: true)
   end
 end
 
 When(/^インターネット上の DNS サーバに dig コマンドで "www.google.com" の IP アドレスを問い合わせる$/) do
   cd('.') do
-    @internet_dns_host.exec "mkdir -p /etc/netns/#{@internet_dns_host.name}"
-    @internet_dns_host.exec "echo '172.217.25.196 www.google.com' | sudo tee /etc/netns/#{@internet_dns_host.name}/hosts"
-    @internet_dns_host.exec "echo 'port=53' | sudo tee dnsmasq.conf"
-    @internet_dns_host.exec "echo 'expand-hosts' | sudo tee -a dnsmasq.conf"
-    @dns_service = AsyncExecutor.new(host: @internet_dns_host, result_file: 'log/dns.log')
-    @dns_service.exec "dnsmasq -C dnsmasq.conf"
-    @src_host.exec "dig @#{@internet_dns_host.ip_address} www.google.com A > log/dns.log"
+    @internet_dns_host.exec("mkdir -p /etc/netns/#{@internet_dns_host.name}", sync: true)
+    @internet_dns_host.exec("echo '172.217.25.196 www.google.com' | sudo tee /etc/netns/#{@internet_dns_host.name}/hosts", sync: true)
+    @internet_dns_host.exec("echo 'port=53' | sudo tee dnsmasq.conf", sync: true)
+    @internet_dns_host.exec("echo 'expand-hosts' | sudo tee -a dnsmasq.conf", sync: true)
+    @dns_service = @internet_dns_host
+    @dns_service.exec("dnsmasq -C dnsmasq.conf")
+    @process_id = @src_host.exec("dig @#{@internet_dns_host.ip_address} www.google.com A", delayed: true)
   end
-end
-
-Then(/^名前解決に成功$/) do
-  step %(the file "log/dns.log" should contain "172.217.25.196")
 end
