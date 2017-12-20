afe5ac05446d088fe926a7a2e15143625ee4e957
qb0C80aE <qb0C80aE@gmail.com>
2017-12-20 17:55:53 +0900

    Modify jenkins_steps to make it work under TestNode

--- features/step_definitions/jenkins_steps.rb
+++ features/step_definitions/jenkins_steps.rb
@@ -1,8 +1,8 @@
 # coding: utf-8
 When(/^Web ブラウザで社内のテスト環境サーバの Web コンソールにアクセス$/) do
   cd('.') do
-    @jenkins_service = AsyncExecutor.new(host: @test_host, result_file: 'log/test_host.log')
-    @jenkins_service.exec "bash -c 'echo -e \"HTTP/1.1 200 OK\\n\\nAccessOK\" | nc -l 13000'"
-    @src_host.exec "curl http://#{@test_host.ip_address}:13000/ > log/access.log"
+    @jenkins_service = @test_host
+    @jenkins_service.exec("bash -c 'echo -e \"HTTP/1.1 200 OK\\n\\nAccessOK\" | nc -l 13000'")
+    @process_id = @src_host.exec("curl http://#{@test_host.ip_address}:13000/", delayed: true)
   end
 end
