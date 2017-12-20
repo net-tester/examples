93d7aaf4fe4c279897f418c744c619dde677b761
qb0C80aE <qb0C80aE@gmail.com>
2017-12-20 17:57:16 +0900

    Modify vpn_steps to make it work under TestNode

--- features/step_definitions/vpn_steps.rb
+++ features/step_definitions/vpn_steps.rb
@@ -1,19 +1,19 @@
 # coding: utf-8
 When(/^VPN サーバに Firewall 経由で http でログイン$/) do
   cd('.') do
-    system "echo '<title>AccessOK</title>' > login.html"
-    @vpn_service = AsyncExecutor.new(host: @vpn_host, result_file: 'log/http_host.stdout', stderr_file: 'log/http_host.stderr')
-    @vpn_service.exec("ruby -rwebrick -e 'WEBrick::HTTPServer.new(:DocumentRoot => \"./\", :Port => 80).start'")
-    @src_host.exec 'curl -L http://203.0.113.5/login.html > log/access.log'
+    @vpn_host.exec("echo '<title>AccessOK</title>' | tee /tmp/login.html", sync: true)
+    @vpn_service = @vpn_host
+    @vpn_service.exec("ruby -rwebrick -e 'WEBrick::HTTPServer.new(:DocumentRoot => \"/tmp\", :Port => 80).start'")
+    @process_id = @src_host.exec('curl -L http://203.0.113.5/login.html', delayed: true)
   end
 end
 
 When(/^VPN サーバに Firewall 経由で https でログイン$/) do
   cd('.') do
-    system "echo '<title>AccessOK</title>' > login.html"
-    @vpn_service = AsyncExecutor.new(host: @vpn_host, result_file: 'log/https_host.stdout', stderr_file: 'log/https_host.stderr')
-    @vpn_service.exec("ruby -rwebrick -rwebrick/https -e 'WEBrick::HTTPServer.new(:DocumentRoot => \"./\", :Port => 443, :SSLEnable => true, :SSLCertName => [[\"CN\", WEBrick::Utils::getservername]] ).start'")
-    @src_host.exec 'curl -L --insecure https://203.0.113.5/login.html > log/access.log'
+    @vpn_host.exec("echo '<title>AccessOK</title>' | tee /tmp/login.html", sync: true)
+    @vpn_service = @vpn_host
+    @vpn_service.exec("ruby -rwebrick -rwebrick/https -e 'WEBrick::HTTPServer.new(:DocumentRoot => \"/tmp\", :Port => 443, :SSLEnable => true, :SSLCertName => [[\"CN\", WEBrick::Utils::getservername]] ).start'")
+    @process_id = @src_host.exec('curl -L --insecure https://203.0.113.5/login.html', delayed: true)
   end
 end
 
