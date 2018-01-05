e04a385a0d5aa1ce8dcbe9edcb1a200b6811d101
qb0C80aE <qb0C80aE@gmail.com>
2018-01-05 14:02:48 +0900

    Fix file creation logic in google_steps

--- features/step_definitions/google_steps.rb
+++ features/step_definitions/google_steps.rb
@@ -1,17 +1,17 @@
 # coding: utf-8
 When(/^ブラウザで Google のページを開く$/) do
   cd('.') do
-    @process_id = @src_host.exec('curl -L --insecure  https://www.google.com/ | iconv -f SHIFT-JIS -t UTF8')
+    @process_id = @src_host.exec('curl -L --insecure https://www.google.com/ | iconv -f SHIFT-JIS -t UTF8')
   end
 end
 
 When(/^ブラウザでインターネット上のサーバの Google のページを開く$/) do
   cd('.') do
-    @internet_host.exec("echo '<title>Google</title>' > index.html", sync: true)
+    @internet_host.exec("rm -f /tmp/index.html; echo '<title>Google</title>' | tee /tmp/index.html", sync: true)
     @https_service = @internet_host
-    @https_service.exec("ruby -rwebrick -rwebrick/https -e 'WEBrick::HTTPServer.new(:DocumentRoot => \"./\", :Port => 443, :SSLEnable => true, :SSLCertName => [[\"CN\", WEBrick::Utils::getservername]] ).start'")
+    @https_service.exec("ruby -rwebrick -rwebrick/https -e 'WEBrick::HTTPServer.new(:DocumentRoot => \"/tmp\", :Port => 443, :SSLEnable => true, :SSLCertName => [[\"CN\", WEBrick::Utils::getservername]] ).start'")
     @src_host.exec("sudo mkdir -p /etc/netns/#{@src_host.name}", sync: true)
     @src_host.exec("echo '198.51.100.3 www.google.com' | sudo tee /etc/netns/#{@src_host.name}/hosts >/dev/null", sync: true)
-    @process_id = @src_host.exec('curl -L --insecure  https://www.google.com/ | iconv -f SHIFT-JIS -t UTF8', delayed: true)
+    @process_id = @src_host.exec('curl -L --insecure https://www.google.com/ | iconv -f SHIFT-JIS -t UTF8', delayed: true)
   end
 end
